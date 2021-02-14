2d0a8812fa954bf273120ceeb8192d8f4f93bea9
Rutujabodkhe77 <rutubodkhe77@gmail.com>
2021-02-14 15:24:13 +0530

    outline button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {Button} from 'react-bootstrap';
+import './Style.css';
+
+
+function App()
+{
+  return(
+    <div className="main_div">
+
+      <div className="Button">     
+     <h1>This Are The Buttons in React-Bootstrap </h1>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+      <Button variant="link">Link</Button>
+      </div>  
+
+  <div className="Button">
+  
+  <h1>This are the Buttons With outline</h1>
+      <Button variant="outline-primary">Primary</Button>{' '}
+      <Button variant="outline-secondary">Secondary</Button>{' '}
+      <Button variant="outline-success">Success</Button>{' '}
+      <Button variant="outline-warning">Warning</Button>{' '}
+      <Button variant="outline-danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+      <Button variant="outline-light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+      <Button variant="outline-link">Link</Button>
+      </div>
+
     </div>
   );
-}
 
+}
 export default App;
